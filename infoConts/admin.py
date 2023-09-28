44e4658b1d1a6a2b6c28be5f55fb2639ab9aa605
AlexanderShemarov <shemarov9822@gmail.com>
2023-09-28 16:33:03 +0300

    A simple last news database with messages is added

--- infoConts/admin.py
+++ infoConts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import LastInfo
 
 # Register your models here.
+
+
+class LastInfoAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "date", "time", "team_staff")
+    search_fields = ["date", "time", "team_staff"]
+
+
+admin.site.register(LastInfo, LastInfoAdmin)
--- infoConts/models.py
+++ infoConts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class LastInfo(models.Model):
+
+    title = models.CharField(max_length=100)
+    message = models.TextField(max_length=2000)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    team_staff = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.title}"
