e02ac13719de12906311bc1c8cbb428c2afad56e
AlexanderShemarov <shemarov9822@gmail.com>
2023-10-13 19:18:09 +0300

    Add model with Team Staff

--- infoConts/admin.py
+++ infoConts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LastInfo
+from .models import LastInfo, Developer
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class LastInfoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(LastInfo, LastInfoAdmin)
+admin.site.register(Developer)
--- infoConts/models.py
+++ infoConts/models.py
@@ -15,3 +15,19 @@ class LastInfo(models.Model):
 
     def __str__(self):
         return f"{self.__class__.__name__} {self.title}"
+
+
+class Developer(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.ForeignKey(
+        User, blank=True, null=True, related_name="nickname", on_delete=models.SET_NULL)
+    email_address = models.TextField(max_length=150)
+    telephones = models.TextField(blank=True, null=True, max_length=100)
+    status_position = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    photo = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
