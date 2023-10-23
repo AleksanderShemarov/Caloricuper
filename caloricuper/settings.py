8354a366599da343d5bd9c402df6a297cafce869
AlexanderShemarov <shemarov9822@gmail.com>
2023-10-24 00:03:36 +0300

    Cors-Headers are added to successful accessings project sources on different domains.

--- caloricuper/settings.py
+++ caloricuper/settings.py
@@ -43,6 +43,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'signInReg.apps.SigninregConfig',
     'shortMain.apps.ShortmainConfig',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 MIDDLEWARE = [
